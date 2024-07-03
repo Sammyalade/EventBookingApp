4bb6153f37bc9a4dfd510e17433aa60057809d54
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-07-03 16:18:17 +0100

    Resources and Others

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO events (id, name, description, date, time)
+VALUES (200, 'Wedding', 'no description', '2024-12-29', '09:00');
+
+
+INSERT INTO attendees (id, first_name, last_name, email, phone, address, city, state, country, have_payed)
+VALUES (100, 'John', 'Doe', 'john.doe@example.com', '123-456-7890', '123 Main St', 'Anytown', 'CA', 'USA', true);
+
